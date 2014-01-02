e5f63bf823dd38d5227cb5fd33ab60151c52de18
diadara <nithin111@gmail.com>
2014-01-03 01:24:18 +0530

    data binding

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,20 @@
-var theData = [ 1,2,3,4]
+var circleRadii = [40, 20, 10];
+var svgContainer = d3.select("#content").append("svg")
+    .attr("width", 200)
+    .attr("height", 200);
+var circles = svgContainer.selectAll("circle")
+    .data(circleRadii)
+    .enter()
+    .append("circle");
+var circleAttributes = circles
+    .attr("cx", 50)
+    .attr("cy", 50)
+    .attr("r", function (d) { return d; })
+    .style("fill", function(d){
+        var returnColor;
+        if (d === 40) { returnColor = "green";
+                      } else if (d === 20) { returnColor = "purple";
+                                           } else if (d === 10) { returnColor = "red"; }
+        return returnColor;
+    });
 
-var p = d3.select("#content").selectAll("p")
-  .data(theData)
-  .enter()
-  .append("p")
-  .text("hello ");
