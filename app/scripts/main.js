a5171741ad38ac831778607795cf3d3d94372024
diadara <nithin111@gmail.com>
2014-01-03 22:05:47 +0530

    basic stuff done, no internet so gave up :(

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,43 @@
-var circleRadii = [40, 20, 10];
-var svgContainer = d3.select("#content").append("svg")
-    .attr("width", 200)
-    .attr("height", 200);
-var circles = svgContainer.selectAll("circle")
-    .data(circleRadii)
-    .enter()
-    .append("circle");
-var circleAttributes = circles
-    .attr("cx", 50)
-    .attr("cy", 50)
-    .attr("r", function (d) { return d; })
-    .style("fill", function(d){
-        var returnColor;
-        if (d === 40) { returnColor = "green";
-                      } else if (d === 20) { returnColor = "purple";
-                                           } else if (d === 10) { returnColor = "red"; }
-        return returnColor;
-    });
+var artists;
 
+function make_visualisation(data)
+{
+var svg =    d3.select("#last").append("svg")
+        .attr("width","100%")
+        .attr("height",1000);
+
+   svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cy", function(d,i){return (i*10);
+                              })
+        .attr("cx", function(d,i){return (i*20);
+                             })
+        .attr("r",function(d){return d.playcount;
+                         })
+    .style("fill",function(){
+        return "hsl("+Math.random()*360+",50%,50%)"}); 
+ 
+    svg.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .attr("y", function(d,i){return (i*10);
+                                })
+        .attr("x", function(d,i){return (i*20);
+                             })
+        .style("text-anchor","middle")
+        .text(function(d){return d.name;
+                         }); 
+
+    //.style("stroke","yellow");
+}
+
+$(function(){
+$.getJSON("/data/last.json",function(data){
+ artists = data.topartists.artist;
+
+    make_visualisation(artists);
+});
+})
