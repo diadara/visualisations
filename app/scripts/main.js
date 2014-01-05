f4a6b685150d21f60583e9c55f209f0996d72c86
diadara <nithin111@gmail.com>
2014-01-05 17:51:49 +0530

    made a simple force layout

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,43 +1,47 @@
-var artists;
+d3.json("data/last.json",function(err,data){
 
-function make_visualisation(data)
-{
-var svg =    d3.select("#last").append("svg")
-        .attr("width","100%")
-        .attr("height",1000);
+var width = 960,
+    height = 500;
 
-   svg.selectAll("circle")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("cy", function(d,i){return (i*10);
-                              })
-        .attr("cx", function(d,i){return (i*20);
-                             })
-        .attr("r",function(d){return d.playcount;
-                         })
-    .style("fill",function(){
-        return "hsl("+Math.random()*360+",50%,50%)"}); 
- 
-    svg.selectAll("text")
-        .data(data)
-        .enter()
-        .append("text")
-        .attr("y", function(d,i){return (i*10);
-                                })
-        .attr("x", function(d,i){return (i*20);
-                             })
-        .style("text-anchor","middle")
-        .text(function(d){return d.name;
-                         }); 
+var fill = d3.scale.category10();
 
-    //.style("stroke","yellow");
-}
-
-$(function(){
-$.getJSON("/data/last.json",function(data){
- artists = data.topartists.artist;
-
-    make_visualisation(artists);
+var nodes = data.topartists.artist.map(function(d,i) {
+  return {index: i,
+         radius: Math.sqrt(d.playcount),
+         image: d.image[1]["#text"]};
 });
-})
+    console.log(nodes);
+
+var force = d3.layout.force()
+    .nodes(nodes)
+    .size([width, height])
+    .on("tick", tick)
+    .start();
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var node = svg.selectAll(".node")
+    .data(nodes)
+  .enter().append("circle")
+    .attr("class", "node")
+    .attr("cx", function(d) { return d.x; })
+    .attr("cy", function(d) { return d.y; })
+    .attr("r", function(d){return d.radius*2;})
+    .style("fill", function(d, i) { return fill(i & 3); })
+    .style("stroke", function(d, i) { return d3.rgb(fill(i & 3)).darker(2); })
+    .call(force.drag);
+
+
+svg.style("opacity", 1e-6)
+  .transition()
+    .duration(1000)
+    .style("opacity", 1);
+
+
+function tick(e) {
+  node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+}
+});
